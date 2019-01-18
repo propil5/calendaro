cdc67b0f265929a7baf8d10227c9723532cdacc5
Piotr Pilch <pilch.piotr5@gmail.com>
2019-01-18 20:01:37 +0100

    Added connection prepered for Azure

--- CalendaroNet/Startup.cs
+++ CalendaroNet/Startup.cs
@@ -38,11 +38,11 @@ namespace CalendaroNet
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            // Use SQL Database if in Azure, otherwise, use SQLite
-            // if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-            //     services.AddDbContext<ApplicationDbContext>(options =>
-            //             options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
-            // else
+            //Use SQL Database if in Azure, otherwise, use SQLite
+            if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+                services.AddDbContext<ApplicationDbContext>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+            else
                 services.AddDbContext<ApplicationDbContext>(options =>
                         options.UseMySql(Configuration.GetConnectionString("LocalConnection")));
 
