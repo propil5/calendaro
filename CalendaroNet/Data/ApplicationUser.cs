3eb531504848d2a9b1d0f597ceff9035fe075d3e
piotra5 <pilch.piotr5@gmail.com>
2019-01-27 19:15:15 +0100

    Added isEmployed bool var for Users for optimalization of employees managment with db migrations.

--- CalendaroNet/Data/ApplicationUser.cs
+++ CalendaroNet/Data/ApplicationUser.cs
@@ -4,4 +4,6 @@ public class ApplicationUser : IdentityUser
 {
     public string Name { get; set; }
     public int Age { get; set; }
+
+    public bool IsEmployed { get; set; }
 }
---
+++ CalendaroNet/Data/Migrations/20190126141357_UpdateUserIsEmployed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CalendaroNet.Data.Migrations
+{
+    public partial class UpdateUserIsEmployed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isEmployed",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isEmployed",
+                table: "AspNetUsers");
+        }
+    }
+}
